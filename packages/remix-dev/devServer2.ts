e0e04ba1eb438aea41c464b47f8d8732e898e6fd
Pedro Cattori <pcattori@gmail.com>
2023-02-03 22:22:01 -0500

    refactor: compiler returns assets manifest and metafiles for browser/server

--- packages/remix-dev/devServer2.ts
+++ packages/remix-dev/devServer2.ts
@@ -11,6 +11,7 @@ import * as Compiler from "./compiler";
 import { type RemixConfig } from "./config";
 import { loadEnv } from "./env";
 import * as LiveReload from "./liveReload";
+import * as HMR from "./hmr";
 
 let info = (message: string) => console.info(`💿 ${message}`);
 
@@ -112,6 +113,7 @@ export let serve = async (
 
   // watch and live reload on rebuilds
   let socket = LiveReload.serve({ port: dev.port });
+  let prevResult: Compiler.CompileResult | undefined = undefined;
   let dispose = await Compiler.watch(config, {
     mode: "development",
     liveReloadPort: dev.port,
@@ -120,8 +122,9 @@ export let serve = async (
       clean(config);
       socket.log("Rebuilding...");
     },
-    onRebuildFinish: async (durationMs, assetsManifest, hmrUpdates) => {
-      if (!assetsManifest) return;
+    onRebuildFinish: async (durationMs, result) => {
+      if (!result) return;
+      let { assetsManifest } = result;
       socket.log(`Rebuilt in ${prettyMs(durationMs)}`);
 
       info(`Waiting for ${appServerOrigin}...`);
@@ -132,14 +135,13 @@ export let serve = async (
         setTimeout(resolve, -1);
       });
 
-      console.log({
-        hmrUpdates,
-      });
-      if (hmrUpdates) {
-        socket.hmr(assetsManifest, hmrUpdates);
+      if (assetsManifest.hmr && prevResult) {
+        let updates = HMR.updates(result, prevResult);
+        socket.hmr(assetsManifest, updates);
         return;
       }
       socket.reload();
+      prevResult = result;
     },
     onFileCreated: (file) => socket.log(`File created: ${relativePath(file)}`),
     onFileChanged: (file) => socket.log(`File changed: ${relativePath(file)}`),
